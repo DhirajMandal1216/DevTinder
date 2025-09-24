ebdf4671fb0b645a3c08d430d0728554f6f20a9b
Dhiraj <dhirajmandal1216@gmail.com>
2025-09-24 22:11:12 +0530

    play with multiple request handelar.

--- src/app.js
+++ src/app.js
@@ -2,17 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("THis is DashBoard");
-});
-
-app.get("/hello", (req, res) => {
-  res.send("THis is Hello");
-});
-
-app.get("/test", (req, res) => {
-  res.send("THis is test ");
-});
+app.use(
+  "/hello",
+  (req, res, next) => {
+      console.log("Response1");
+    //   res.send("THis is Hello 1");
+    next();
+  },
+  (req, res, next) => {
+      console.log("Response2");
+    //   res.send("THis is Hello 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Response1");
+    res.send("THis is Hello 3");
+  }
+);
 
 app.listen(7000, () => {
   console.log("Server is running on port 7777..");
