c4bbf7711f2e7f2dd1132577563f6751ea544611
Dhiraj <dhirajmandal1216@gmail.com>
2025-09-24 23:28:38 +0530

    wildcard and middl ware error handling

--- src/app.js
+++ src/app.js
@@ -18,9 +18,16 @@ app.get("/user2", (req, res) => {
   }
 });
 
-app.use("/", (err, req, res, next) => {
+// Wildcard route for 404(NOt found)
+app.get(/.*/, (req, res) => {
+  res.status(404).send("404 Not Found");
+});
+
+// Error handling middleware
+app.use( (err, req, res, next) => {
   res.status(500).send("Something went wrong.");
 });
+
 
 app.listen(7000, () => {
   console.log("Server is running on port 7777..");
